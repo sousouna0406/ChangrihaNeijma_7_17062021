4f6abed1751f245620b4305e1a82741e0dbb7d4d
changriha neijma <changriha.neijma@orange.fr>
2022-01-18 17:14:35 +0100

    route Auth

---
+++ backend/routes/authRoute.js
@@ -0,0 +1,8 @@
+const { Router } = require("express");
+const router = Router();
+
+// Route pour l'inscription d'un user
+router.post("/signup");
+// Route pour l'autentification d'un user
+router.post("/login");
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,18 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const express = require("express");
 const bodyParser = require("body-parser");
+const authRouting = require("./routes/authRoute");
+
+dotenv.config({
+  path: "config/.env",
+});
 
 const app = express();
 
-app.use("/api/auth");
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use("/api/auth", authRouting);
 
 app.listen(process.env.PORT || 3000);
