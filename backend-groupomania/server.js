1bd48657e0aa1041e9d2528115693e1388350bcd
changriha neijma <changriha.neijma@orange.fr>
2022-01-25 20:28:10 +0100

    server.js

--- backend-groupomania/server.js
+++ backend-groupomania/server.js
@@ -2,7 +2,10 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const express = require("express");
 const bodyParser = require("body-parser");
-const authRouting = require("./routes/authRoute");
+
+const authRouting = require("./routes/authRoutes");
+const postRouting = require("./routes/postRoutes");
+const userRouting = require("./routes/userRoutes");
 
 dotenv.config({
   path: "config/.env",
@@ -14,6 +17,8 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use("/api/post", postRouting);
 app.use("/api/auth", authRouting);
+app.use("/api/user", userRouting);
 
 app.listen(process.env.PORT || 3000);
