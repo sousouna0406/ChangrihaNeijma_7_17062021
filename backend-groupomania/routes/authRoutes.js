5bfb74365391e852278a7261e3566057ee2fb812
changriha neijma <changriha.neijma@orange.fr>
2022-01-26 00:50:43 +0100

    commentaires

--- backend-groupomania/routes/authRoutes.js
+++ backend-groupomania/routes/authRoutes.js
@@ -3,9 +3,7 @@ const { signup, login } = require("../controllers/authControllers");
 const authParams = require("../middlewares/authParams");
 const router = Router();
 
-// Route pour l'inscription d'un user
 router.post("/signup", authParams, signup);
-// Route pour l'autentification d'un user
 router.post("/login", authParams, login);
 
 module.exports = router;
