d209cb658c9e37dd4cc36a3438a7df72f695ad91
changriha neijma <changriha.neijma@orange.fr>
2022-01-26 23:47:16 +0100

    bbd liaison

--- backend-groupomania/server.js
+++ backend-groupomania/server.js
@@ -1,32 +1,29 @@
-const express = require('express');
+const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config({
-    path: "config/.env",
-})
+  path: "config/.env",
+});
 const bodyParser = require("body-parser");
-const { initDb } = require('./config/dbConfig');
-const extraSetup = require('./config/extraSetup');
-const authRoutes = require('./routes/authRoutes');
-const postRoutes = require('./routes/postRoutes');
-const userRoutes = require('./routes/userRoutes');
+const { initDb } = require("./config/dbConfig");
+const extraSetup = require("./config/extraSetup");
+const authRoutes = require("./routes/authRoutes");
+const postRoutes = require("./routes/postRoutes");
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 
-
 console.log(1);
-
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
-
 initDb();
 extraSetup();
 
-app.use('/api/auth', authRoutes);
-app.use('/api/posts', postRoutes);
-app.use('/api/users', userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/users", userRoutes);
 
-app.listen(3006);
+app.listen(3000);
