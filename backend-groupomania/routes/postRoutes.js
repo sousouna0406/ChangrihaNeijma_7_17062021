48f236bbd79064974f2608313a8f67a3c62c7405
changriha neijma <changriha.neijma@orange.fr>
2022-02-14 09:01:09 +0100

    ajout multer middelwares

--- backend-groupomania/routes/postRoutes.js
+++ backend-groupomania/routes/postRoutes.js
@@ -8,6 +8,7 @@ const {
 } = require("../controllers/postController");
 const isOwnerOrAdmin = require("../middlewares/isOwnerOrAdmin");
 const token = require("../middlewares/token");
+const multer = require("../middlewares/multer");
 
 const router = Router();
 // route pour la récupération de tout les posts
@@ -15,9 +16,9 @@ router.get("/", token, getAllPosts);
 // route pour la récupération d'un seul posts (non utiliser)
 router.get("/:id", token, getOnePost);
 // route pour création d'un post
-router.post("/", token, createOnePost);
+router.post("/", token, multer, createOnePost);
 // route pour la modification d'un post
-router.put("/:id", token, isOwnerOrAdmin, updateOnePost);
+router.put("/:id", token, isOwnerOrAdmin, multer, updateOnePost);
 // route pour la suppression d'un post
 router.delete("/:id", token, isOwnerOrAdmin, deleteOnePost);
 
